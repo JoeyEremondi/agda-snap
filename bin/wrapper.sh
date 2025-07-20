6225f4d3027c023c07b62ad01d08b04cea3c5582
Joey Eremondi <joey.eremondi@gmail.com>
2025-07-20 17:01:46 -0600

    Initial commit

---
+++ bin/wrapper.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+echo "Current snap files:"
+ls $SNAP
+for f in $SNAP
+do
+    echo "listing $f"
+    ls $f
+done
+
+#  Assign the agda directories to defaults if they're not already set
+if [ -z "${AGDA_DIR+x}" ]; then
+  export AGDA_DIR=$SNAP/agda-dir
+fi
+
+if [ -z "${Agda_datadir+x}" ]; then
+  mkdir -p $SNAP_USER_COMMON/agda_data
+  export Agda_datadir=$SNAP_USER_COMMON/agda_data
+fi
+
+
+# Make temp directory outside of the snap's container fs
+mkdir -p $SNAP_USER_COMMON/tmp
+export TMPDIR=$SNAP_USER_COMMON/tmp
+
+# Make directory to put compile artifacts
+mkdir -p $SNAP_USER_COMMON/_build
+
+
+$SNAP/bin/agda --compile-dir="$SNAP_USER_COMMON/_build" "$@"
